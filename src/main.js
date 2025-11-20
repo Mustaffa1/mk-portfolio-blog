3e36a0da8dcc740b5a07adf93169d50505a3a7a1
Mustaffa1 <mustafakoparal298@gmail.com>
2025-11-20 19:41:18 +0300

    feat: Add scroll animations (AOS) and typewriter effect

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // <-- Router'ı içeri aktar
 import i18n from './i18n'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const app = createApp(App)
 
 app.use(router) // <-- Uygulamaya router'ı ekle
 app.use(i18n)
 app.mount('#app')
+app.mount('#app')
+AOS.init({
+  duration: 800, // Animasyon süresi (ms)
+  once: true,    // Animasyon sadece bir kere çalışsın (aşağı inip çıkınca tekrar etmesin)
+  offset: 50,    // Tetiklenme mesafesi
+});
